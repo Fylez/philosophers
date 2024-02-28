7d03a0e05f0177323a6a40dbdcb05944ff08cfa2
fylez <superliamzaengel@gmail.com>
2024-02-28 18:28:04 +0100

    mutex

--- main.c
+++ main.c
@@ -6,38 +6,48 @@
 /*   By: lzaengel <lzaengel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 15:52:45 by lzaengel          #+#    #+#             */
-/*   Updated: 2024/02/27 17:23:45 by lzaengel         ###   ########.fr       */
+/*   Updated: 2024/02/28 18:27:10 by lzaengel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
 
-void	*test()
+void	*test(void *philo)
 {
+	pthread_mutex_lock(&((t_philo *)philo)->table->lock);
+	printf("%d\n",((t_philo *)philo)->index);
 	printf("test\n");
 	printf("test2\n");
 	printf("test3\n");
+	pthread_mutex_unlock(&((t_philo *)philo)->table->lock);
 }
-void	init_threads(int nphilos, pthread_t *philos)
+void	init_threads(int nphilos, t_philo *philos, t_table lock)
 {
 	int i;
-	pthread_mutex
 	i = 0;
 	while(i <= nphilos)
 	{
-		pthread_create(&philos[i], NULL, test, NULL);
+		philos[i].table = &lock;
+		philos[i].index = i;
+		pthread_create(&(philos[i].philo), NULL, test, &philos[i]);
+		i++;
+	}
+	i = 0;
+	while(i <= nphilos)
+	{
+		pthread_join(philos[i].philo, NULL);
 		i++;
 	}
 }
 int	main(int argc, char *argv[])
 {
-	pthread_t	*philos;
+	t_table lock;
 	long int	nphilos;
 	long int	tdie;
 	long int	teat;
 	long int	tsleep;
-
+	t_philo		*philos;
 	if (argc < 5 || argc > 6)
 		ft_exit("Wrong numbers of arguments");
 	check_args(argc, argv);
@@ -45,6 +55,7 @@ int	main(int argc, char *argv[])
 	tdie = ft_atoi(argv[2]);
 	teat = ft_atoi(argv[3]);
 	tsleep = ft_atoi(argv[4]);
-	philos = malloc(sizeof(pthread_t) * nphilos);
-	init_threads(nphilos, philos);
+	philos = malloc(sizeof(t_philo) * nphilos);
+	pthread_mutex_init(&lock.lock, NULL);
+	init_threads(nphilos, philos, lock);
 }
--- philosophers.h
+++ philosophers.h
@@ -6,7 +6,7 @@
 /*   By: lzaengel <lzaengel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 16:20:19 by lzaengel          #+#    #+#             */
-/*   Updated: 2024/02/27 18:54:04 by lzaengel         ###   ########.fr       */
+/*   Updated: 2024/02/28 18:11:57 by lzaengel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@
 # include <stdio.h>
 # include <pthread.h>
 
+typedef struct s_table {
+	pthread_mutex_t lock;
+}	t_table;
+
 typedef struct s_philo {
-	t_mute
-	pthread
+	int index;
+	t_table *table;
+	pthread_t philo;
 }	t_philo;
 
 long	ft_atoi( const char *theString);
